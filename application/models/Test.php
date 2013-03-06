6b9dfd4aef3a24379c691c0236f3d92e48e72cea
Jaime <jrodev@yahoo.es>
2013-03-05 22:21:43 -0500

    model test

---
+++ application/models/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Test
+ *
+ * @author jrodev
+ */
+class Application_Model_Test {
+    //put your code here
+}
+
+?>
