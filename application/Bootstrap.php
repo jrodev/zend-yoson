7401791f818c923cef4f6256175769f10bc55bf3
Jaime Rodriguez <jaime.rodriguez@perucomsultores.pe>
2013-04-03 20:02:45 -0500

    integration with bootstrap twitter

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,17 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    public function __construct($appIni){ //echo "APP:"; var_dump($app); exit();
+        
+        parent::__construct($appIni);
+        $paths = $this->getOption('paths');
+        defined('STAT_URL') || define('STAT_URL', $paths['statUrl']);
+        defined('BASE_URL') || define('BASE_URL', $paths['baseUrl']);
+        defined('CSS_URL')  || define('CSS_URL' , $paths['cssUrl']);
+        defined('JS_URL')   || define('JS_URL'  , $paths['jsUrl']);
+        
+        $pars=$this->getOption('pars'); defined('MIN')||define('MIN',$pars['min']);
+        
+    }
 
 }
-
