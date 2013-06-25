dcbba364b5812a55cadc036de5744d3d43f9d9bd
jaime R <jrodev@yahoo.es>
2013-06-25 08:36:30 -0500

    configurnado en host - change public a public_html

--- application/modules/admin/Bootstrap.php
+++ application/modules/admin/Bootstrap.php
@@ -2,9 +2,7 @@
 
 class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
 {
-
     public function __construct($appIni){ //echo "APP:"; var_dump($app); exit();
-        echo "Admin_Bootstrap";
         parent::__construct($appIni);
         /*$paths = $this->getOption('paths');
         defined('STAT_URL')     || define('STAT_URL'    , $paths['statUrl']);
@@ -12,9 +10,6 @@ class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
         defined('CSS_URL')      || define('CSS_URL'     , $paths['cssUrl']);
         defined('JS_URL')       || define('JS_URL'      , $paths['jsUrl']);
         defined('PORTAL_VIEWS') || define('PORTAL_VIEWS', $paths['portalViews']);
-        
         $pars=$this->getOption('pars'); defined('MIN')||define('MIN',$pars['min']);*/
-        
     }
-
 }
